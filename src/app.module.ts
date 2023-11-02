aea42602cf5a9d5a67a6885b348715366e3f3591
a-maged <maged.dev@gmail.com>
2023-11-02 06:35:12 +0200

    move config into separate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
+import { CustomConfigModule } from './custom-config/custom-config.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test')
-          .default('development'),
-        PORT: Joi.number().default(3000),
-        DATABASE_USER: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
-      }),
-    }),
-  ],
+  imports: [CustomConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/custom-config/custom-config.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+const envSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+  DATABASE_USER: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
+});
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: envSchema,
+    }),
+  ],
+  exports: [ConfigModule],
+})
+export class CustomConfigModule {}
