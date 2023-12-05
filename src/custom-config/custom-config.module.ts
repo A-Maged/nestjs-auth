3e8ec9507e97a5d65b2065f8387dbb7b59f946d3
a-maged <maged.dev@gmail.com>
2023-12-05 02:16:05 +0200

    require FRONTEND_APP_URL to env vars

--- src/custom-config/custom-config.module.ts
+++ src/custom-config/custom-config.module.ts
@@ -5,6 +5,7 @@ import * as Joi from 'joi';
 const envSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
   PORT: Joi.number().default(3000),
+  FRONTEND_APP_URL: Joi.string().required(),
   DATABASE_HOST: Joi.string().required(),
   DATABASE_PORT: Joi.string().default(5432),
   DATABASE_USERNAME: Joi.string().required(),
