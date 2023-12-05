f85f2197cbe596125381e84899de58f31899f0e2
a-maged <maged.dev@gmail.com>
2023-12-05 02:44:51 +0200

    just formatting migration files

--- src/database/migrations/1701737031995-default-avatar-url.ts
+++ src/database/migrations/1701737031995-default-avatar-url.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DefaultAvatarUrl1701737031995 implements MigrationInterface {
-    name = 'DefaultAvatarUrl1701737031995'
+  name = 'DefaultAvatarUrl1701737031995';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "client" ALTER COLUMN "avatar" SET DEFAULT 'https://placehold.co/200x200'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "client" ALTER COLUMN "avatar" SET DEFAULT 'https://placehold.co/200x200'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "client" ALTER COLUMN "avatar" SET DEFAULT 'http://default-url'`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "client" ALTER COLUMN "avatar" SET DEFAULT 'http://default-url'`);
+  }
 }
